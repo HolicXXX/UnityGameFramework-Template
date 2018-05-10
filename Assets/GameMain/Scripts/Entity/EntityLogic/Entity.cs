f0b57c5588b0c14ed7731f257860fec1f6cdc1da
Allen <allendk@foxmail.com>
2018-05-10 19:51:15 +0800

    update to Unity 2018.1

--- Assets/GameMain/Scripts/Entity/EntityLogic/Entity.cs
+++ Assets/GameMain/Scripts/Entity/EntityLogic/Entity.cs
@@ -23,13 +23,13 @@ namespace GameMain
             private set;
         }
 
-        protected internal override void OnInit(object userData)
+        protected override void OnInit(object userData)
         {
             base.OnInit(userData);
             CachedAnimation = GetComponent<Animation>();
         }
 
-        protected internal override void OnShow(object userData)
+        protected override void OnShow(object userData)
         {
             base.OnShow(userData);
 
@@ -46,32 +46,32 @@ namespace GameMain
             CachedTransform.localScale = Vector3.one;
         }
 
-        protected internal override void OnHide(object userData)
+        protected override void OnHide(object userData)
         {
             base.OnHide(userData);
         }
 
-        protected internal override void OnAttached(EntityLogic childEntity, Transform parentTransform, object userData)
+        protected override void OnAttached(EntityLogic childEntity, Transform parentTransform, object userData)
         {
             base.OnAttached(childEntity, parentTransform, userData);
         }
 
-        protected internal override void OnDetached(EntityLogic childEntity, object userData)
+        protected override void OnDetached(EntityLogic childEntity, object userData)
         {
             base.OnDetached(childEntity, userData);
         }
 
-        protected internal override void OnAttachTo(EntityLogic parentEntity, Transform parentTransform, object userData)
+        protected override void OnAttachTo(EntityLogic parentEntity, Transform parentTransform, object userData)
         {
             base.OnAttachTo(parentEntity, parentTransform, userData);
         }
 
-        protected internal override void OnDetachFrom(EntityLogic parentEntity, object userData)
+        protected override void OnDetachFrom(EntityLogic parentEntity, object userData)
         {
             base.OnDetachFrom(parentEntity, userData);
         }
 
-        protected internal override void OnUpdate(float elapseSeconds, float realElapseSeconds)
+        protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
             base.OnUpdate(elapseSeconds, realElapseSeconds);
         }
--- Assets/GameMain/Scripts/UI/UGuiForm.cs
+++ Assets/GameMain/Scripts/UI/UGuiForm.cs
@@ -68,7 +68,7 @@ namespace GameMain
             Destroy(go);
         }
 
-        protected internal override void OnInit(object userData)
+        protected override void OnInit(object userData)
         {
             base.OnInit(userData);
 
@@ -97,7 +97,7 @@ namespace GameMain
             }
         }
 
-        protected internal override void OnOpen(object userData)
+        protected override void OnOpen(object userData)
         {
             base.OnOpen(userData);
 
@@ -106,17 +106,17 @@ namespace GameMain
             StartCoroutine(m_CanvasGroup.FadeToAlpha(1f, FadeTime));
         }
 
-        protected internal override void OnClose(object userData)
+        protected override void OnClose(object userData)
         {
             base.OnClose(userData);
         }
 
-        protected internal override void OnPause()
+        protected override void OnPause()
         {
             base.OnPause();
         }
 
-        protected internal override void OnResume()
+        protected override void OnResume()
         {
             base.OnResume();
 
@@ -125,27 +125,27 @@ namespace GameMain
             StartCoroutine(m_CanvasGroup.FadeToAlpha(1f, FadeTime));
         }
 
-        protected internal override void OnCover()
+        protected override void OnCover()
         {
             base.OnCover();
         }
 
-        protected internal override void OnReveal()
+        protected override void OnReveal()
         {
             base.OnReveal();
         }
 
-        protected internal override void OnRefocus(object userData)
+        protected override void OnRefocus(object userData)
         {
             base.OnRefocus(userData);
         }
 
-        protected internal override void OnUpdate(float elapseSeconds, float realElapseSeconds)
+        protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
             base.OnUpdate(elapseSeconds, realElapseSeconds);
         }
 
-        protected internal override void OnDepthChanged(int uiGroupDepth, int depthInUIGroup)
+        protected override void OnDepthChanged(int uiGroupDepth, int depthInUIGroup)
         {
             int oldDepth = Depth;
             base.OnDepthChanged(uiGroupDepth, depthInUIGroup);
